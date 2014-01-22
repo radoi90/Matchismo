d2b430a14df26ec002fc652e53270de1c80e8992
radoi90 <eculic17@gmail.com>
2014-01-22 19:53:46 +0000

    added setters for set card members

--- Machismo/Model/SetCard.m
+++ Machismo/Model/SetCard.m
@@ -20,6 +20,21 @@
     return 0;
 }
 
+- (void)setNumber:(NSUInteger)number
+{
+    if (number <= [SetCard maxNumber]) {
+        _number = number;
+    }
+}
+
+- (void)setShading:(NSString *)shading
+{
+    if ([[SetCard validShadings] containsObject:shading])
+    {
+        _shading = shading;
+    }
+}
+
 + (NSArray *)validShadings{
     return @[@"solid", @"outlined", @"striped"];
 }
