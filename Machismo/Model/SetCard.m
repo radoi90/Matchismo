7d2b5ac6d538bd34576d7771039bd57a855ebbf3
radoi90 <eculic17@gmail.com>
2014-01-22 22:44:06 +0000

    matching set cards

--- Machismo/Model/SetCard.m
+++ Machismo/Model/SetCard.m
@@ -11,13 +11,39 @@
 
 @implementation SetCard
 
+// 3 set cards match when, for each attribute, they are all equal
+// or all have different values (pair-wise distinct)
 - (int)match:(NSArray *)otherCards
 {
-    if ([otherCards count] == [SetCard maxNumber] -1) {
-        
+    if ([otherCards count] != [SetCard maxNumber] -1) return 0;
+    
+    SetCard *otherCard0 = otherCards[0];
+    SetCard *otherCard1 = otherCards[1];
+    
+    if (!(([self.color isEqualToString:otherCard0.color] && [self.color isEqualToString:otherCard1.color]) ||
+          (![self.color isEqualToString:otherCard0.color] && ![otherCard0.color isEqualToString:otherCard1.color]
+           && ![otherCard1.color isEqualToString:self.color]))) {
+        return 0;
+          }
+    
+    if (!(([self.shape isEqualToString:otherCard0.shape] && [self.shape isEqualToString:otherCard1.shape]) ||
+          (![self.shape isEqualToString:otherCard0.shape] && ![otherCard0.shape isEqualToString:otherCard1.shape]
+           && ![otherCard1.shape isEqualToString:self.shape]))) {
+              return 0;
+          }
+    
+    if (!(([self.shading isEqualToString:otherCard0.shading] && [self.shading isEqualToString:otherCard1.shading]) ||
+          (![self.shading isEqualToString:otherCard0.shading] && ![otherCard0.shading isEqualToString:otherCard1.shading]
+           && ![otherCard1.shading isEqualToString:self.shading]))) {
+              return 0;
+          }
+    
+    if (!((self.number == otherCard0.number && self.number == otherCard1.number) ||
+         (self.number != otherCard0.number && otherCard0.number != otherCard1.number && otherCard1.number != self.number))) {
+        return 0;
     }
     
-    return 0;
+    return 1;
 }
 
 - (void)setNumber:(NSUInteger)number
